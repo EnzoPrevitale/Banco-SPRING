3879ac542fcb12fc792c595be3fc7a58c7dd253f
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-07-22 19:46:26 -0300

    CRUD completo para usuários

--- src/main/java/com/magu/bank/controllers/UserController.java
+++ src/main/java/com/magu/bank/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.magu.bank.controllers;
 
 import com.magu.bank.dtos.UserDto;
 import com.magu.bank.models.User;
-import com.magu.bank.repositories.UserRepository;
 import com.magu.bank.services.UserService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -19,16 +19,44 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    // Obter todos
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.status(HttpStatus.OK).body(service.getUsers());
     }
 
+    // Obter por ID
+    @GetMapping("/{id}")
+    public ResponseEntity getUser(@PathVariable Integer id) {
+        Optional<User> user = service.getUser(id);
+        if(user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
+    }
+
+    // Salvar novo
     @PostMapping
     public ResponseEntity<User> postUser(@RequestBody UserDto dto) {
         User user = new User();
         BeanUtils.copyProperties(dto, user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(service.validateUser(user));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.encryptPassword(user));
     }
 
+    // Editar existente
+    @PutMapping("/{id}")
+    public ResponseEntity putUser(@PathVariable Integer id, @RequestBody UserDto dto) {
+        Optional<User> user = service.getUser(id);
+        if(user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Not Found");
+        }
+        BeanUtils.copyProperties(dto, user.get());
+        return ResponseEntity.status(HttpStatus.OK).body(service.encryptPassword(user.get()));
+    }
+
+    // Apagar registro
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.deleteUser(id));
+    }
 }
--- src/main/java/com/magu/bank/services/UserService.java
+++ src/main/java/com/magu/bank/services/UserService.java
@@ -3,11 +3,14 @@ package com.magu.bank.services;
 import com.magu.bank.models.User;
 import com.magu.bank.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,22 +23,25 @@ public class UserService {
         return repository.findAll();
     }
 
-    // Regra de negócio: armazenar senhas criptografadas, verificar CPF
-    public User validateUser(User user) throws Exception {
-        // Valida CPF
-        String cpf = user.getCpf().replaceAll("[^0-9]", "");
-        if(cpf.length() == 11) {
-            String lastTwo = cpf.substring(cpf.length() - 2 );
-            for(int i = 0; i < cpf.length(); i++) {
-                int c = (int)(cpf.charAt(cpf.length() - i));
-                
-            }
-        } else {
-            throw new Exception("CPF lenght must equal 11.");
+    // Obter por ID
+    public Optional<User> getUser(Integer id) {
+        return repository.findById(id);
+    }
+
+    // Apagar usuário
+    public User deleteUser(Integer id) {
+        Optional<User> user = getUser(id);
+        if(user.isPresent()) {
+            User userModel = user.get();
+            repository.delete(userModel);
+            return userModel;
+        }else {
+            throw new RuntimeException("User not found");
         }
+    }
 
-
-
+    // Regra de negócio: armazenar senhas criptografadas
+    public User encryptPassword(User user) {
         // Criptografa senha
         String password = user.getPassword();
         String newPassword;
@@ -55,8 +61,7 @@ public class UserService {
             newPassword = hexString.toString();
 
         } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
+            throw new RuntimeException("An error ocurred  while trying to encrypt the password.");
         }
 
         user.setPassword(newPassword);
